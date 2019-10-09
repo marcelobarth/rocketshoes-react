10b19566d49bee77bc9e37e485e71d58926c660a
marcelobarth <marcelinhobarth@hotmail.com>
2019-10-09 13:29:53 -0300

    Criando Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import Routes from "./routes";
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
-  return (
-    <BrowserRouter>
-      {/* <Header /> */}
-      <Routes />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes />
+            <GlobalStyle />
+        </BrowserRouter>
+    );
 }
 
 export default App;
