259495f331a051240b846a01f42c2310a491df4a
marcelobarth <marcelinhobarth@hotmail.com>
2019-10-11 13:27:19 -0300

    Estilizacao da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <div />;
+    return (
+        <ProductList>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/56/HZM-1731-056/HZM-1731-056_detalhe1.jpg?resize=280:280"
+                    alt="Tênis"
+                />
+                <strong>Tênis Legal</strong>
+                <span>R$130,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/56/HZM-1731-056/HZM-1731-056_detalhe1.jpg?resize=280:280"
+                    alt="Tênis"
+                />
+                <strong>Tênis Legal</strong>
+                <span>R$130,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/56/HZM-1731-056/HZM-1731-056_detalhe1.jpg?resize=280:280"
+                    alt="Tênis"
+                />
+                <strong>Tênis Legal</strong>
+                <span>R$130,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/56/HZM-1731-056/HZM-1731-056_detalhe1.jpg?resize=280:280"
+                    alt="Tênis"
+                />
+                <strong>Tênis Legal</strong>
+                <span>R$130,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/56/HZM-1731-056/HZM-1731-056_detalhe1.jpg?resize=280:280"
+                    alt="Tênis"
+                />
+                <strong>Tênis Legal</strong>
+                <span>R$130,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/56/HZM-1731-056/HZM-1731-056_detalhe1.jpg?resize=280:280"
+                    alt="Tênis"
+                />
+                <strong>Tênis Legal</strong>
+                <span>R$130,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
