aed03219335b31ffea24a1f9f39d84798e99baae
marcelobarth <marcelinhobarth@hotmail.com>
2019-10-16 13:46:07 -0300

    bind json.server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+/* json-server -H 0.0.0.0 -p 3000 -w server.json */
 const api = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'http://localhost:3000',
 });
 
 export default api;
